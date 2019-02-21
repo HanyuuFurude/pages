b24001cf0eb426ead2e90f1ecd721b450bc8349a
Hanyuu <HanyuuFurude@outlook.com>
2019-02-21 11:46:57 +0800

    201902211146

--- source/_posts/spider/requests/getBinaryData(github).py
+++
@@ -1,9 +0,0 @@
-import requests
-
-res = requests.get("https://github.com/favicon.ico")
-print(res.text)
-print(res.content)
-with open('favicon.ico','wb') as favicon:
-	favicon.write(res.content)
-with open('favicon.ico') as exp:
-	print(exp.read())
